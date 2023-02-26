885866281c743a2f7edffc9a9a1720080f9c7483
Anna Bryzgalova <annabryzgalova1984@gmail.com>
2023-02-26 18:34:21 +0300

    draft_1

--- src/js/GameState.js
+++ src/js/GameState.js
@@ -1,4 +1,11 @@
 export default class GameState {
+  constructor() {
+    // Начальный уровень
+    this.level = 1;
+    // Делаем все ячейки игрового поля массивом
+    this.allCell = [];
+  }
+
   static from(object) {
     // TODO: create object
     return null;
--- src/js/Team.js
+++ src/js/Team.js
@@ -11,6 +11,24 @@
  * team.characters // [swordsman, bowman]
  * ```
  * */
+import Bowman from './Characters/Bowman';
+import Swordsman from './Characters/Swordsman';
+import Daemon from './Characters/Daemon';
+import Undead from './Characters/Undead';
+import Vampire from './Characters/Vampire';
+import Magician from './Characters/Magician';
+
 export default class Team {
   // TODO: write your logic here
+  constructor() {
+    this.userHero = [Bowman, Swordsman, Magician];
+  }
+
+  static playerHeroes() {
+    return [Magician, Bowman, Swordsman];
+  }
+
+  static aiHeroes() {
+    return [Daemon, Undead, Vampire];
+  }
 }
